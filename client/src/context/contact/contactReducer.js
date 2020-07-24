d2aa3a6e433d6462362e70fa09ccfbe048eff3bb
Tyler Casperson <tylercasperson@gmail.com>
2020-07-23 23:04:12 -0600

    contact form can filter

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -40,9 +40,18 @@ export default (state, action) => {
         ),
       };
     case FILTER_CONTACTS:
-      return {};
+      return {
+        ...state,
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, 'gi');
+          return contact.name.match(regex) || contact.email.match(regex);
+        }),
+      };
     case CLEAR_FILTER:
-      return {};
+      return {
+        ...state,
+        filtered: null,
+      };
     default:
       return state;
   }
